7f0db6ad8ebf2a61ba54b7d6e7491eeb42909694
Andrei <vrasmas_vasy@yahoo.com>
2021-02-11 15:44:58 +0000

    Backend server with routes, controllers and personalized Error model

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const placeRoutes = require("./routes/places-routes");
+
+const app = express();
+
+app.use("/api/places", placeRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "Unknown error occured..." });
+});
+
+app.listen(5000, () => {
+  console.log(`Server running on port 5000...`);
+});
