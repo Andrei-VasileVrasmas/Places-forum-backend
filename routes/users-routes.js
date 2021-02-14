232cf5d9644b7a1c8fa4406db065ba17d22d1c61
Andrei <vrasmas_vasy@yahoo.com>
2021-02-14 01:55:49 +0000

    using google geocoding API

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const usersController = require("../controllers/users-controller");
 
@@ -6,7 +7,15 @@ const router = express.Router();
 
 router.get("/", usersController.getUsers);
 
-router.post("/signup", usersController.signUp);
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersController.signUp
+);
 
 router.post("/login", usersController.login);
 
